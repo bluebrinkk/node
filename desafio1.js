b8189fb8f2e7c50e7a2e6f262b57a447156d345d
Esteban Diaz <bluebrinkk@gmail.com>
2021-07-12 23:51:10 -0400

    Desafio 1 realizado

--- desafio1.js
+++ desafio1.js
@@ -1,11 +1,46 @@
-const { json } = require("express");
-
-let texto = 'Node.js es un entorno en tiempo de ejecución multiplataforma, de código abierto, para la capa del servidor basado en el lenguaje de programación JavaScript, asíncrono, con E/S de datos en una arquitectura orientada a eventos y basado en el motor V8 de Google.';
-
-// Cuantas veces aparecen las siguientes palabras
+/*  Cuantas veces aparecen las siguientes palabras en el texto
 Node
 javascript
 entorno
-tiempo
+tiempo */
 
 // Quitar puntos y parentecis en la busqueda
+
+let texto = 'Node. js es un entorno de tiempo de ejecución de JavaScript (de ahí su terminación en . js haciendo alusión al lenguaje JavaScript). Este entorno de tiempo de ejecución en tiempo real incluye todo lo que se necesita para ejecutar un programa escrito en JavaScript.';
+// Formatea texto
+texto = texto.replace(/,/g, '').replace(/\./g, '').replace(/\)/g, '').replace(/\(/g, '').replace(/[ ]+/g, ' ').toLowerCase();
+// Separa palabras retornando un arreglo
+texto = texto.split(' ');
+
+// -------------------------------------------------------------------------------------
+console.log('Solucion 1');
+
+let node = 0;
+let javascript = 0;
+let entorno = 0;
+let tiempo = 0;
+
+texto.forEach(element => {
+    if (element == 'node') node++;
+    else if (element == 'javascript') javascript++;
+    else if (element == 'entorno') entorno++;
+    else if (element == 'tiempo') tiempo++;
+});
+
+console.log(node);
+console.log(javascript);
+console.log(entorno);
+console.log(tiempo);
+
+// -------------------------------------------------------------------------------------
+console.log('Solucion 2');
+
+const nodeS = texto.filter(el => el == 'node');
+const javaS = texto.filter(el => el == 'javascript');
+const entoS = texto.filter(el => el == 'entorno');
+const tiemS = texto.filter(el => el == 'tiempo');
+
+console.log(nodeS.length);
+console.log(javaS.length);
+console.log(entoS.length);
+console.log(tiemS.length);
